806077fc7e60f18688412dff010ecd5cd4e0b6c0
Mark-Liu <1152729525@qq.com>
2022-05-16 18:00:39 +0800

    Add files via upload

---
+++ non_w_DenseNet/model/se_block.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#   https://openaccess.thecvf.com/content_cvpr_2018/html/Hu_Squeeze-and-Excitation_Networks_CVPR_2018_paper.html
+
+class SEBlock(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel//reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel//reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class LSE(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(LSE, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel//reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel//reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, l, d = x.size()
+        # y = x.view(b*l, d)
+        y = self.fc(x)
+        return x + y.expand_as(x)
